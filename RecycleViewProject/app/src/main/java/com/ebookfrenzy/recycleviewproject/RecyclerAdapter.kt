685401d93f4dc654d58395dffcc863ad8ab143e8
Jim DiMaggio <jdimaggio32@gmail.com>
2022-03-30 18:34:30 -0400

    update

---
+++ RecycleViewProject/app/src/main/java/com/ebookfrenzy/recycleviewproject/RecyclerAdapter.kt
@@ -0,0 +1,56 @@
+package com.ebookfrenzy.recycleviewproject
+
+import android.view.LayoutInflater
+import android.widget.ImageView
+import android.widget.TextView
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.ebookfrenzy.recycleviewproject.R
+import com.google.android.material.snackbar.Snackbar
+
+//need a save state for when device is rotated
+
+class RecyclerAdapter : RecyclerView.Adapter<RecyclerAdapter.ViewHolder>() {
+
+
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        var itemImage: ImageView
+        var itemTitle: TextView
+        var itemDetail: TextView
+        init {
+            itemImage = itemView.findViewById(R.id.itemImage)
+            itemTitle = itemView.findViewById(R.id.itemTitle)
+            itemDetail = itemView.findViewById(R.id.itemDetail)
+
+            itemView.setOnClickListener { v: View ->
+                var position: Int = getAdapterPosition()
+                Snackbar.make(v, "Click detected on item $position",
+                    Snackbar.LENGTH_LONG).setAction("Action", null).show()
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(viewGroup: ViewGroup, i: Int): ViewHolder {
+        val v = LayoutInflater.from(viewGroup.context)
+            .inflate(R.layout.card_layout, viewGroup, false)
+        return ViewHolder(v)
+    }
+
+    fun  ranNum(): Int {
+        val random1 = (0..7).shuffled().last()
+        return random1
+    }
+
+    override fun onBindViewHolder(viewHolder: ViewHolder, i: Int) {
+
+        viewHolder.itemTitle.text = Data().getTitles()[ranNum()]
+        viewHolder.itemDetail.text = Data().getDetails()[ranNum()]
+        viewHolder.itemImage.setImageResource(Data().getImages()[ranNum()])
+    }
+
+    override fun getItemCount(): Int {
+        return Data().getTitles().size
+    }
+
+}
